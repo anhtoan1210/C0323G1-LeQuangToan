6c6cae1980cd7b3091107e210f1ad5ea227e284f
anhtoan1210 <lequangtoan12102000@gmail.com>
2023-05-09 20:46:56 +0700

    thay doi

--- module2/src/ss2_array_loop/bai_tap/HienThiCacHinh.java
+++ module2/src/ss2_array_loop/bai_tap/HienThiCacHinh.java
@@ -10,15 +10,25 @@ public class HienThiCacHinh {
             }
             System.out.print("\n");
         }
-        for (int i = 0; i <=5 ; i++) {
+        for (int i = 0; i <= 5; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println("\n");
         }
-        for (int i = 7; i >=1 ; i--) {
-            for (int j = 1; j <i ; j++) {
+        for (int i = 7; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
                 System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= 10; j++) {
+                if (j > 5 - i && j < 5 + i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println("\n");
         }
