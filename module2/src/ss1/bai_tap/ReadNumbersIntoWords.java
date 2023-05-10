a530529def10913f6c249d8faf3fae697ebad4e4
anhtoan1210 <lequangtoan12102000@gmail.com>
2023-05-10 17:11:07 +0700

    thay đổi

--- module2/src/ss1/bai_tap/ReadNumbersIntoWords.java
+++ module2/src/ss1/bai_tap/ReadNumbersIntoWords.java
@@ -2,7 +2,7 @@ package ss1.bai_tap;
 
 import java.util.Scanner;
 
-public class DocSoThanhChu {
+public class ReadNumbersIntoWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number;
