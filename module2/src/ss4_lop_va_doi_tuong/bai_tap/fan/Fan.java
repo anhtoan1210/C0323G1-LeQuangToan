19211a0b25233d43e4e0b1ca1b38df427e31e278
anhtoan1210 <lequangtoan12102000@gmail.com>
2023-05-12 06:41:00 +0700

    bai tap

--- module2/src/ss4_lop_va_doi_tuong/bai_tap/fan/Fan.java
+++ module2/src/ss4_lop_va_doi_tuong/bai_tap/fan/Fan.java
@@ -1,19 +1,60 @@
 package ss4_lop_va_doi_tuong.bai_tap.fan;
 
-public class Fan {
-    int SLOW = 1;
-    int MEDIUM = 2;
-    int FAST = 3;
-    int speed;
-    boolean on;
-    double radius;
-    String color;
 
+    public class Fan {
+        public static final int SLOW = 1;
+        public static final int MEDIUM = 2;
+        public static final int FAST = 3;
 
-    public Fan(int speed, boolean on, double radius, String color) {
-        this.speed = speed;
-        this.on = on;
-        this.radius = radius;
-        this.color = color;
+        private int speed;
+        private boolean on;
+        private double radius;
+        private String color;
+
+        public Fan() {
+            this.speed = SLOW;
+            this.on = false;
+            this.radius = 5;
+            this.color = "blue";
+        }
+
+        public int getSpeed() {
+            return speed;
+        }
+
+        public void setSpeed(int speed) {
+            this.speed = speed;
+        }
+
+        public boolean isOn() {
+            return on;
+        }
+
+        public void setOn(boolean on) {
+            this.on = on;
+        }
+
+        public double getRadius() {
+            return radius;
+        }
+
+        public void setRadius(double radius) {
+            this.radius = radius;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public String toString() {
+            if (this.on) {
+                return "Fan is on: " + "speed " + this.speed + ", color " + this.color + ", radius " + this.radius;
+            } else {
+                return "Fan is off: " + "color " + this.color + ", radius " + this.radius;
+            }
+        }
     }
-}
