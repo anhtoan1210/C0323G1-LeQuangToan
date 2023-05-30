9330e2a177393b98500c5936a11076305b41b6f4
anhtoan1210 <lequangtoan12102000@gmail.com>
2023-05-30 07:00:03 +0700

    bai tap

--- module2/src/ss17_io_binary_file_serialization/bai_tap_2/Main.java
+++ module2/src/ss17_io_binary_file_serialization/bai_tap_2/Main.java
@@ -1,4 +1,24 @@
 package ss17_io_binary_file_serialization.bai_tap_2;
 
+import ss17_io_binary_file_serialization.bai_tap_1.model.Product;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("chọn đường link muốn sao chép");
+        String path = scanner.nextLine();
+        List<Product> stringList = ReadAndWrite.readTofile(path);
+        for (Product p : stringList) {
+            System.out.println(p);
+        }
+        System.out.println("nhập đờng link muốn gửi tới");
+        String pathG = scanner.nextLine();
+        ReadAndWrite.writeListToFile(pathG,stringList);
+        System.out.println("ok");
+    }
+
 }
+
--- module2/src/ss17_io_binary_file_serialization/bai_tap_2/ReadAndWrite.java
+++ module2/src/ss17_io_binary_file_serialization/bai_tap_2/ReadAndWrite.java
@@ -1,9 +1,47 @@
 package ss17_io_binary_file_serialization.bai_tap_2;
 
+import ss17_io_binary_file_serialization.bai_tap_1.model.Product;
+
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class ReadAndWrite {
-    public static void writeListToFile(String path) {
+public class ReadAndWrite implements Serializable {
+    public static void writeListToFile(String path, List<Product> stringList) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(path);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(stringList);
+            objectOutputStream.flush();
+            objectOutputStream.close();
+            fileOutputStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("s");
+            ;
+        }
 
     }
+
+    public static List<Product> readTofile(String path) {
+        List<Product> productList = new ArrayList<>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(path);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            productList = (List<Product>) objectInputStream.readObject();
+            objectInputStream.close();
+            fileInputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("sao chép file");
+            System.out.printf(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return productList;
+    }
 }
+
